c4b297ce7878cffc74f9fe2dd97ff8e7364898d9
Your Name <alanmayer@email.arizona.edu>
2020-10-27 15:21:21 -0700

    push

--- module-6-starter/module-6-starter/assets/js/homepage.js
+++ module-6-starter/module-6-starter/assets/js/homepage.js
@@ -3,12 +3,45 @@ var getUserRepos = function (user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     //make a request to the URL
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            displayRepos(data, user);
+
+            //clear old content
+            repoContainerEl.textContent = "";
+            repoSearchTerm.textContent = searchTerm;
         });
     });
 };
 
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
 
-getUserRepos();
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    console.log(event);
+
+    //get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    }
+    else {
+        alert("Please enter a GitHub username");
+    }
+
+};
+
+var displayRepos = function (repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+};
+
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+
+
+userFormEl.addEventListener("submit", formSubmitHandler);
