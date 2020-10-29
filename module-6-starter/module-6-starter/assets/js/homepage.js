aa1d8b2c4079242a798b6e71df286a07c8baf3f4
Your Name <alanmayer@email.arizona.edu>
2020-10-28 19:22:56 -0700

    push

--- module-6-starter/module-6-starter/assets/js/homepage.js
+++ module-6-starter/module-6-starter/assets/js/homepage.js
@@ -61,8 +61,12 @@ var displayRepos = function (repos, searchTerm) {
         var repoName = repos[i].owner.login + "/" + repos[i].name;
 
         //create a container for each repo
-        var repoEl = document.createElement("div");
+        // Here we are also creating a link for each repo
+        var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
+        // This connects out single-repo.html to our index.html
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+
 
         // create a span element to hold repository name
         var titleEl = document.createElement("span");
